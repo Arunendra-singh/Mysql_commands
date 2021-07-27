0e7f17153518b02480cba32eef30fbab789bd160
Arunendra Singh <arunendrasingh11@live.com>
2021-07-27 12:18:54 +0530

    Create column_in_table.sql

---
+++ column_in_table.sql
@@ -0,0 +1,17 @@
+When you are trying to add new column in your existing database table : 
+
+# try this code + read comments carefully 
+
+
+
+alter table wp_terms add 'what_ever_column_name' datatype ();     // where as wp_terms is demo table name,    and
+                                                                  // reple datatype what ever you want (like - int, varchar)
+                                                                  
+                                                                  
+                                                                                                                                    
+                                                                  
+When you are trying to remove new column in your existing database table : 
+
+# try this code
+
+alter table wp_terms drop 'column_name'; 
