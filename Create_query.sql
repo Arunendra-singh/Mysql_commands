2d9eb089d89ea06df990305873015ddcbd11d3c6
Arunendra Singh <arunendrasingh11@live.com>
2021-07-27 13:19:57 +0530

    Update Create_query.sql

--- Create_query.sql
+++ Create_query.sql
@@ -15,3 +15,8 @@ CREATE TABLE Persons (PersonID int, PersonName varchar(50) );
 ## When you trying to delete table using mysqli :
 
 DROP TABLE table_name;
+
+
+
+## When you trying to delete database using mysqli :
+DROP DATABASE db_name;
