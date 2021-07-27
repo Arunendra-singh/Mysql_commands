c7f67ceac20ffaac3a736433c01e8984044e84a7
Arunendra Singh <arunendrasingh11@live.com>
2021-07-27 13:07:16 +0530

    Update Create_query.sql

--- Create_query.sql
+++ Create_query.sql
@@ -1,3 +1,17 @@
 ## When you trying to create database using mysqli :
 
 CREATE DATABASE IF NOT EXISTS tempdb;    
+
+
+
+## When you trying to create table using mysqli :
+
+
+CREATE TABLE Persons
+(
+PersonID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+);
