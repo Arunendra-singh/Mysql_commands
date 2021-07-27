63924a0c95351d6dc1c7b3bb546f9a85520dfa52
Arunendra Singh <arunendrasingh11@live.com>
2021-07-27 13:36:29 +0530

    Update Data_update.sql

--- Data_update.sql
+++ Data_update.sql
@@ -1,4 +1,13 @@
 ## if you need to update your existing table data than follow : 
 
+
+UPDATE persons SET PersonName = 'Arunendra Singh'
+WHERE PersonId = 101;
+
+
+or)
+
+
 UPDATE employees SET lastname = 'Singh', email = 'arun.singh@arunclass.com'
 WHERE employeeNumber = 101;
+
